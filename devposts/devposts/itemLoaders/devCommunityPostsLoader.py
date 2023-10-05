6f8062ffab42f4f8b346c47a366f7cb7914f5237
Gregory Vicent <gregoryvicent.dev@gmail.com>
2023-10-05 18:37:59 -0400

    007: Geted info to devCommunity website and clean data

--- devposts/devposts/itemLoaders/devCommunityPostsLoader.py
+++ devposts/devposts/itemLoaders/devCommunityPostsLoader.py
@@ -2,4 +2,8 @@ from itemloaders.processors import TakeFirst, MapCompose
 from scrapy.loader import ItemLoader
 
 class DevCommunityPostsLoader(ItemLoader):
-    pass
+    default_output_processor = TakeFirst()
+    title_in = MapCompose(lambda x: x[15:-13]) # Limpiamos el titulo de cada post
+    # link_in = MapCompose()
+    # date_in = MapCompose()
+
--- devposts/devposts/spiders/devcommunity_spider.py
+++ devposts/devposts/spiders/devcommunity_spider.py
@@ -1,21 +1,26 @@
 import scrapy
-from devposts.items import DevCommunityPostItem
+from devposts.itemLoaders.devCommunityPostsLoader import DevCommunityPostsLoader # ItemLoader para el analisis de los datos
+from devposts.items import DevCommunityPostItem # Esquema para los datos de este Spider
 
+# Spider que hace web scraping en 'https://dev.to/'
 class DevCommunitySpider(scrapy.Spider):
     name = "devCommunity"
     start_urls = ["https://dev.to/"]
-    DevCommunityPost = DevCommunityPostItem()
 
     def parse(self, response):
+        # Extraemos  los links de la pagina principal sobre los que buscaremos la información
         links = response.css("main div.crayons-story a.crayons-story__hidden-navigation-link::attr(href)").getall()
         for link in links:
             if link is not None:
-                yield response.follow(link, callback=self.get_post_info)
+                yield response.follow(link, callback=self.get_post_info) # Entramos en cada post
 
     def get_post_info(self, response):
-        self.DevCommunityPost["title"] = response.css("h1::text").get()
-        self.DevCommunityPost["date"] = response.css("time::attr(datetime)").get()
-        self.DevCommunityPost["link"] = response.url
-        yield self.DevCommunityPost
-        yield scrapy.Request("https://dev.to/", callback=self.parse)
+        # Aquí ya estamos dentro de cada uno de los posts
+        main = response.css("main")
+        post = DevCommunityPostsLoader(item=DevCommunityPostItem(), selector=main)
+        post.add_css("title", "h1::text")
+        post.add_css("date", "time::attr(datetime)")
+        post.add_value("link", response.url)
+        yield post.load_item()
+        yield scrapy.Request(response.url, callback=self.parse) # Regresamos a la pagina principal de 'https://dev.to/'
 
