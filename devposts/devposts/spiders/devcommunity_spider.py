3146b18e68b8e1750e5f773036347eaa3b96f7e6
Gregory Vicent <gregoryvicent.dev@gmail.com>
2023-10-05 17:37:11 -0400

    006: Integrate Items for 'DevCommunityPostItem'

--- devposts/devposts/spiders/devcommunity_spider.py
+++ devposts/devposts/spiders/devcommunity_spider.py
@@ -1,8 +1,10 @@
 import scrapy
+from devposts.items import DevCommunityPostItem
 
 class DevCommunitySpider(scrapy.Spider):
     name = "devCommunity"
     start_urls = ["https://dev.to/"]
+    DevCommunityPost = DevCommunityPostItem()
 
     def parse(self, response):
         links = response.css("main div.crayons-story a.crayons-story__hidden-navigation-link::attr(href)").getall()
@@ -11,10 +13,9 @@ class DevCommunitySpider(scrapy.Spider):
                 yield response.follow(link, callback=self.get_post_info)
 
     def get_post_info(self, response):
-        data_post = {
-            "title": response.css("h1::text").get(),
-            "date": response.css("time::attr(datetime)").get(),
-            "link": response.url
-        }
+        self.DevCommunityPost["title"] = response.css("h1::text").get()
+        self.DevCommunityPost["date"] = response.css("time::attr(datetime)").get()
+        self.DevCommunityPost["link"] = response.url
+        yield self.DevCommunityPost
         yield scrapy.Request("https://dev.to/", callback=self.parse)
 
