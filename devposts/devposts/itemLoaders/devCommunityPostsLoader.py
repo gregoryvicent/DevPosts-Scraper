7620b5309319e2cca7c7dd3296a0d397e61333ab
Gregory Vicent <gregoryvicent.dev@gmail.com>
2023-10-06 18:01:19 -0400

    A few change

--- devposts/devposts/itemLoaders/devCommunityPostsLoader.py
+++ devposts/devposts/itemLoaders/devCommunityPostsLoader.py
@@ -4,6 +4,4 @@ from scrapy.loader import ItemLoader
 class DevCommunityPostsLoader(ItemLoader):
     default_output_processor = TakeFirst()
     title_in = MapCompose(lambda x: x[15:-13]) # Limpiamos el titulo de cada post
-    # link_in = MapCompose()
-    # date_in = MapCompose()
 
